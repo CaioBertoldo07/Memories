b011d0b6ccb2e0db42f1148f2076b51b004cb4b3
Caio Bertoldo <caio23bezerra@gmail.com>
2025-04-03 12:09:11 -0400

    Conclusão backend

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -1,4 +1,15 @@
 const Memory = require("../models/Memory");
+const fs = require("fs")
+
+const removeOldImage = (memory) => {
+  fs.unlink(`public/${memory.src}`, (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Image deleted from server!")
+    }
+  })
+}
 
 const createMemory = async (req, res) => {
   try {
@@ -23,6 +34,131 @@ const createMemory = async (req, res) => {
   }
 };
 
+const getMemories = async (req, res) => {
+  try {
+    const memories = await Memory.find()
+
+    res.json(memories)
+  } catch (error) {
+    res.status(500).send("There was an error!")
+  }
+}
+
+const getMemory = async (req, res) => {
+  try {
+    const memory = await Memory.findById(req.params.id)
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memory not found" })
+    }
+
+    res.json(memory)
+  } catch (error) {
+    res.status(500).send("There was an error!")
+  }
+}
+
+const deleteMemory = async (req, res) => {
+  try {
+    const memory = await Memory.findByIdAndRemove(req.params.id)
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memory not found" })
+    }
+
+    removeOldImage(memory)
+
+    res.json({ msg: "Deleted memory" })
+  } catch (error) {
+    res.status(500).send("There was an error!")
+  }
+}
+
+const updateMemory = async (req, res) => {
+  try {
+    const { title, description } = req.body
+    
+    let src = null
+
+    if (req.file) {
+      src = `images/${req.file.filename}`
+    }
+
+    const memory = await Memory.findById(req.params.id)
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memory not found" })
+    }
+
+    if (src) {
+      removeOldImage(memory)
+    }
+
+    const uptadeData = {}
+
+    if (title) uptadeData.title = title
+    if (description) uptadeData.description = description
+    if (src) uptadeData.src = src
+    
+    const updateMemory = await Memory.findByIdAndUpdate(req.params.id, uptadeData, { new: true })
+
+    res.json({ updateMemory, msg: "Memory updated successfully!" })
+  } catch (error) {
+    res.status(500).send("There was an error!")
+  }
+}
+
+const toggleFavorite = async (req, res) => {
+  try {
+    const memory = await Memory.findById(req.params.id)
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memory not found" })
+    }
+
+    memory.favorite = !memory.favorite
+
+    await memory.save()
+
+    res.json({ msg: "Added to favorites", memory })
+  } catch (error) {
+    res.status(500).send("There was an error!")
+  }
+
+}
+
+const addComment = async (req, res) => {
+  try {
+    const { name, text } = req.body
+
+    if (!name || !text) {
+      return res.status(400).json({ msg: "Please, fill in all fields." })
+    }
+
+    const comment = { name, text }
+
+    const memory = await Memory.findById(req.params.id)
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memory not found" })
+    }
+
+    memory.comments.push(comment)
+
+    await memory.save()
+
+    res.json({ msg: "Comment added", memory })
+  } catch (error) {
+    res.status(500).send("There was an error!")
+  }
+}
+
 module.exports = {
   createMemory,
+  getMemories,
+  getMemory,
+  deleteMemory,
+  updateMemory,
+  toggleFavorite,
+  addComment,
 };
--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const upload = require("./helpers/upload");
 
-const { createMemory } = require("./controllers/MemoryController");
+const { createMemory, getMemories, getMemory, deleteMemory, updateMemory, toggleFavorite, addComment } = require("./controllers/MemoryController");
 
 router.post(
   "/",
@@ -17,5 +17,17 @@ router.post(
   },
   (req, res) => createMemory(req, res)
 );
+
+router.get("/", (req, res) => getMemories(req, res))
+
+router.get("/:id", (req, res) => getMemory(req, res))
+
+router.delete("/:id", (req, res) => deleteMemory(req, res))
+
+router.patch("/:id", upload.single("image"), (req, res) => updateMemory(req, res))
+
+router.patch("/favorite/:id", (req, res) => toggleFavorite(req, res))
+
+router.patch("/:id/comment", (req, res) => addComment(req, res))
 
 module.exports = router;
