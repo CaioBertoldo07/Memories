30a5fca86b644c65c5e9047f10854bfa2bdd13c5
Caio Bertoldo <caio23bezerra@gmail.com>
2025-09-09 10:46:19 -0400

    .

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,13 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const express = require("express")
+const app = express()
+const cors = require("cors")
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+app.use(cors())
+app.use(express.json())
+app.use(express.static("public"))
 
-require("./db/conn");
-
-const port = 3000;
-
-const memoryRoutes = require("./routes");
-app.use("/memories", memoryRoutes);
+const port = 3000
 
 app.listen(port, async () => {
-  console.log(`The server started on port: ${port}`);
-});
+    console.log(`The server started on the port ${port}`)
+})
--- frontend/src/axios-config.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-axios.defaults.baseURL = "http://localhost:3000"
-
-axios.defaults.headers.post["Content-Type"] = "application/json"
-
-axios.defaults.timeout = 10000
-
-export default axios
